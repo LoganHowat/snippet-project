02915ad3f8241165c552b3fdc0b4ac4ce84245ea
Logan Howat <lh31@sanger.ac.uk>
2023-07-12 11:53:38 +0100

    added encryption and decryption

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
+from flask_bcrypt import Bcrypt
 from seedData import seedData
 import json
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
 
 @app.get('/snippets')
 def get_all_snippets():
@@ -21,3 +24,17 @@ def add_snippet():
     request_data["id"] = len(seedData) + 1
     seedData.append(request_data)
     return (seedData)
+
+@app.post('/encrypt')
+def hash_password():
+    request_data = json.loads(request.data)
+    hashed_password = bcrypt.generate_password_hash(request_data["password"], 18)
+    return (hashed_password)
+
+@app.post('/decrypt')
+def test_hash_and_password():
+    request_data = json.loads(request.data)
+    is_valid = bcrypt.check_password_hash(request_data["hash"], request_data["password"])
+    return ({"match": is_valid})
+
+# Pass the data in form {hash: asdjfbasjdfhs, password: password}
